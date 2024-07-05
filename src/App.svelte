903d0c80813821bbadf6bc4fcd088f63f7a31856
Logan <Cattn@mathstudy.dev>
2024-07-05 01:26:45 -0400

    erm

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
     
 </script>
 
-<div class="flex justify-center">
+<div class="flex justify-center bg-gray-900 rounded-lg p-4">
 <p class="text-sm">{real}</p>
 <p class="text-lg"> ermmm! </p>
 </div>
