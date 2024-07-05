6cfd9cc91afad372d2dde23c10c80dd0275acb1e
Logan <Cattn@mathstudy.dev>
2024-07-05 00:57:06 -0400

    Push + Refactor

---
+++ rollup.config.Foo.mjs
@@ -0,0 +1,42 @@
+import svelte from 'rollup-plugin-svelte'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import css from 'rollup-plugin-css-only'
+
+const production = !process.env.ROLLUP_WATCH
+
+// include CSS in component's JS for ease of use
+//
+// set to true to get separate CSS for the component (but then,
+// you'll need to inject it yourself at runtime somehow)
+//
+const emitCss = false
+
+const cmp = 'Foo'
+
+export default {
+  input: `src/${cmp}.svelte`,
+
+  output: {
+    format: 'es',
+    file: `dist/${cmp}.js`,
+    sourcemap: false,
+  },
+
+  plugins: [
+    svelte({
+      emitCss,
+      compilerOptions: {
+        dev: !production,
+      },
+    }),
+
+    emitCss && css({ output: `${cmp}.css` }),
+
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+  ],
+}
---
+++ src/Foo.svelte
@@ -0,0 +1,8 @@
+<script>
+    let real = "real";
+    export const name = "name";
+    export const ermm = "ermm";
+</script>
+
+
+<p>{real}</p>
